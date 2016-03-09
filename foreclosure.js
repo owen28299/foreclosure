488dce0c201ad87fb0dd247155e7ec3a336bed52
Owen <owenyangg@gmail.com>
2016-03-09 12:47:28 -1000

    Owen finished

--- foreclosure.js
+++ foreclosure.js
@@ -1,4 +1,4 @@
-'strict mode';
+'use strict';
 
 var steve;
 var stevesLoan;
@@ -35,8 +35,53 @@ function loan() {
     getMonthlyPayment: function(){
       return account.monthlyPayment;
     },
-
-
+    isForeclosed: function(){
+      return account.foreclosed;
+    },
+    numDefaulted: function(){
+        return account.defaulted;
+    }
   };
 
+}
+
+
+function borrower(loan){
+    var account = {
+      monthlyIncome: 1350,
+      funds: 2800,
+      loan: loan
+    };
+
+    return {
+      getFunds: function(){
+        return account.funds;
+      },
+      makePayment: function(){
+        if(account.funds > loan.getMonthlyPayment()) {
+          loan.receivePayment(loan.getMonthlyPayment());
+          account.funds -= loan.getMonthlyPayment();
+        }
+        else {
+        loan.receivePayment(account.funds);
+        account.funds = 0;
+        }
+      },
+      payDay: function(){
+        account.funds += account.monthlyIncome;
+      },
+      getMonthlyIncome: function(){
+        return account.monthlyIncome;
+      }
+    };
+}
+
+var stevesLoan = loan();
+var steve = borrower(stevesLoan);
+monthsUntilEvicted = steve.getFunds() / (stevesLoan.getMonthlyPayment() - steve.getMonthlyIncome()) + 5;
+
+while(stevesLoan.isForeclosed() === false && stevesLoan.getBalance() > 0){
+  steve.payDay();
+  steve.makePayment();
+  ++month;
 }
